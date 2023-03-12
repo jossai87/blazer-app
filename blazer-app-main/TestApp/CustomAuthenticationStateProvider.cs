c842ab0b56c6e24a4da8889748c83d4e7e495162
jossai87 <jossai6@gatech.edu>
2023-03-12 08:34:04 -0500

    Redid Lesson_9 and fixed issues

--- blazer-app-main/TestApp/CustomAuthenticationStateProvider.cs
+++ blazer-app-main/TestApp/CustomAuthenticationStateProvider.cs
@@ -1,57 +1,56 @@
-using System.Security.Claims;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Authorization;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Components.Authorization;
 
 
 
-public class CustomAuthenticationStateProvider: AuthenticationStateProvider {
+    public class CustomAuthenticationStateProvider: AuthenticationStateProvider {
 
-//Return Auth state
-public override async Task<AuthenticationState> GetAuthenticationStateAsync(){
-    return await Task.FromResult(new AuthenticationState(AnonymousUser));
-}
-
-//Ability to check Auth sstate (sign in / sign out)
-private ClaimsPrincipal AnonymousUser => new(new ClaimsIdentity(Array.Empty<Claim>()));
-
-//delcare fake user
-private ClaimsPrincipal RegularFakdeUser{
-    get{
-        var claims = new[] {
-            new Claim(ClaimTypes.Name, "john"),
-            new Claim(ClaimTypes.Role, "user"),
-        };
-        var identity = new ClaimsIdentity(claims, "faked");
-        return new ClaimsPrincipal(identity);
-
+    //Return Auth state
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync(){
+        return await Task.FromResult(new AuthenticationState(AnonymousUser));
     }
-}
 
-private ClaimsPrincipal AdminFakedUser{
-    get{
-        var claims = new[] {
-            new Claim(ClaimTypes.Name, "sally"),
-            new Claim(ClaimTypes.Role, "admin"),
-        };
-        var identity = new ClaimsIdentity(claims, "faked");
-        return new ClaimsPrincipal(identity);
+    //Ability to check Auth sstate (sign in / sign out)
+    private ClaimsPrincipal AnonymousUser => new(new ClaimsIdentity(Array.Empty<Claim>()));
+
+    //delcare fake user
+    private ClaimsPrincipal RegularFakedUser{
+        get{
+            var claims = new[] {
+                new Claim(ClaimTypes.Name, "john"),
+                new Claim(ClaimTypes.Role, "user"),
+            };
+            var identity = new ClaimsIdentity(claims, "faked");
+            return new ClaimsPrincipal(identity);
+
+        }
     }
-}
 
-//Methods for signing in and out
-public void RegularFakedSignIn(){
-    var results = Task.FromResult(new AuthenticationState(RegularFakdeUser));
-    NotifyAuthenticationStateChanged(result);
-}
+    private ClaimsPrincipal AdminFakedUser{
+        get{
+            var claims = new[] {
+                new Claim(ClaimTypes.Name, "sally"),
+                new Claim(ClaimTypes.Role, "admin"),
+            };
+            var identity = new ClaimsIdentity(claims, "faked");
+            return new ClaimsPrincipal(identity);
+        }
+    }
 
-public void AdminFakedSignIn(){
-    var results = Task.FromResult(new AuthenticationState(AdminFakdeUser));
-    NotifyAuthenticationStateChanged(result);
-}
+    //Methods for signing in and out
+    public void RegularFakedSignIn(){
+        var results = Task.FromResult(new AuthenticationState(RegularFakedUser));
+        NotifyAuthenticationStateChanged(results);
+    }
 
-public void SignOut(){
-    var results = Task.FromResult(new AuthenticationState(AnonymousUser));
-    NotifyAuthenticationStateChanged(result);
-}
+    public void AdminFakedSignIn(){
+        var results = Task.FromResult(new AuthenticationState(AdminFakedUser));
+        NotifyAuthenticationStateChanged(results);
+    }
 
-} 
+    public void SignOut(){
+        var results = Task.FromResult(new AuthenticationState(AnonymousUser));
+        NotifyAuthenticationStateChanged(results);
+    }
+
+    } 
